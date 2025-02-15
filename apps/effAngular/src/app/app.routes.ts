0695bba8cfc8b017ad70f82d2ac465901d25ec0c
maciek <maciek29.opozda@gmail.com>
2025-02-15 22:49:46 +0100

    ROUTING-1 - Cleanup main component - Add Two routes for expenses-overview, and expenses-approval pages

--- apps/effAngular/src/app/app.routes.ts
+++ apps/effAngular/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
+import { ExpensesOverviewPageComponent } from '../../page/expensesOverviewPage/expenses-overview-page.component';
+import { ExpensesApprovalPageComponent } from '../../page/expensesApprovalPage/expenses-approval-page.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: 'expenses-overview', component: ExpensesOverviewPageComponent },
+  { path: 'expenses-approval', component: ExpensesApprovalPageComponent },
+];
